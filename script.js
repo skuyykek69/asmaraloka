d60bb200f7784bbadb8707ebf0f114a9db922e35
Wananaaaaaaaa <muhammadikhwanus26@gmail.com>
2024-08-13 02:54:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('.fade-in');
+    
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
